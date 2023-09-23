b66dcae4905e9585911f820d69ec5f74782f1081
Coralie Fonta <fonta.coralie@hotmail.fr>
2023-09-24 01:41:39 +0200

    Update du CSS, JS, et création de la page d'inscription

--- layout.js
+++ layout.js
@@ -1,7 +1,27 @@
 // JS du LAYOUT
 /* --------- */
 
-// Fonction de chargement du contenu de fichiers HTML vers d'autres fichiers
+// Fonction qui permet de charger le CSS du layout
+function loadCSS(){
+    const cssLayout = document.createElement("link");
+    cssLayout.rel = "stylesheet";
+    cssLayout.href = "layout.css";
+    document.head.appendChild(cssLayout);
+}
+
+// Fonction qui va permettre d'appliquer la classe "active" selon la page où l'utilisateur se trouve
+function setNavBarActiveItem(){
+    const navElements = document.querySelectorAll(".nav-link");
+    const currentURL = window.location.href;
+    navElements.forEach(navLink =>{
+        if(navLink.href === currentURL){
+            navLink.classList.add("active");
+            navLink.setAttribute("aria-current", "page");
+        }
+    });
+}
+
+// Enfin, la fonction qui va permettre de charger TOUT le layout
 function loadContent(file, targetElement){
     fetch(file)
         .then(response =>{
@@ -9,14 +29,14 @@ function loadContent(file, targetElement){
         })
         .then(data =>{
             targetElement.innerHTML = data;
+            loadCSS();
+            setNavBarActiveItem();
         })
         .catch(error =>{
             console.error(`Une erreur est survenue lors du chargement: ${error}`);
         });
 }
-// Chargement du contenu de la navbar (nav.html)
-let headerElement = document.querySelector("header");
+const headerElement = document.querySelector("header");
 loadContent("nav.html", headerElement);
-// Chargement du contenu du footer (footer.html)
-let footerElement = document.querySelector("footer");
+const footerElement = document.querySelector("footer");
 loadContent("footer.html", footerElement);
